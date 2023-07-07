f52b585e3390c09bb1d62a16cb098d84f3937330
Keong96 <chinkeongtan96@gmail.com>
2023-07-07 22:52:10 +0800

    add jtw token

--- index.js
+++ index.js
@@ -24,3 +24,38 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: false, parameterLimit:5
 app.listen(PORT, () => {
   console.log(`listening on ${PORT}`);
 });
+
+function GenerateJWT(_userId, _email, _username)
+{
+  return jwt.sign(
+      { userId: _userId, email: _email, username: _username},
+      process.env.TOKEN_KEY,
+      { expiresIn: "24h" }
+    );
+}
+
+function verifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader) {
+    const token = authHeader.split(" ")[1];
+    jwt.verify(token, process.env.TOKEN_KEY, (err, user) =>
+    {
+      if (err)
+      {
+        return res.sendStatus(403);
+      }
+
+      req.user = user;
+      next();
+    });
+  }
+  else
+  {
+    res.sendStatus(401);
+  }
+}
+
+app.get('/', async (req, res) => {
+    res.status(200).send("OK");
+  })
